2f57b6fa2c732fa9bac17cfddaab293ec5e2e6d5
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2022-02-25 16:43:50 +0200

    feat: finish movie details endpoint

--- app/src/main/java/com/example/foodtruck/data/api/MovieDetailsEndPoint.kt
+++ app/src/main/java/com/example/foodtruck/data/api/MovieDetailsEndPoint.kt
@@ -1,5 +1,24 @@
 package com.example.foodtruck.data.api
 
+import com.example.foodtruck.data.model.credits.MovieDetailsCredits
+import com.example.foodtruck.data.model.movie.MovieDetails
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface MovieDetailsEndPoint {
 
+    @GET("/3/movie/{movie_id}")
+    fun getMovieDetails(
+        @Query("api_key") token: String,
+        @Path("movie_id") movieId: Int
+    ): Call<MovieDetails>
+
+    @GET("/3/movie/{movie_id}/credits")
+    fun getMovieCredits(
+        @Query("api_key") token: String,
+        @Path("movie_id") movieId: Int
+    ): Call<MovieDetailsCredits>
 }
